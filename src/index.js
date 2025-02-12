d1751a7472463736a605fbf8a8fa2f066e266d08
aaquibaftab <aaquibaftab7903@gmail.com>
2025-02-12 14:54:48 +0530

    Connecting to Postman, All setup configured

--- src/index.js
+++ src/index.js
@@ -1,51 +1,21 @@
-// require('dotenv').config({path: './env'})
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import express from "express";
+import {app} from './app.js';
 
-dotenv.config({
-    path: './env'
-})
+// Load environment variables
+dotenv.config({ path: './env' });
+
 
 
 
 connectDB()
-.then (()=>{
-    app.listen(process.env.PORT || 8000,()=>{
-        console.log(`Server is Running at Port: ${process.env.port }`)
+    .then(() => {
+        const port = process.env.PORT || 8001; // Use environment variable or default
+        app.listen(port, () => {
+            console.log(`Server is running at Port: ${port}`);
+        });
     })
-    })
-
-.catch ((err) => {
-    console.log("MONGODB connection failed!!!", err);
-})
-
-
-
-
-
-
-
-
-
-/*
-import express from "express"
-const app = express()
-( async () => {
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        app.on("errror", (error) => {
-            console.log("ERRR: ", error);
-            throw error
-        })
-
-        app.listen(process.env.PORT, () => {
-            console.log(`App is listening on port ${process.env.PORT}`);
-        })
-
-    } catch (error) {
-        console.error("ERROR: ", error)
-        throw err
-    }
-})()
-
-*/
+    .catch((err) => {
+        console.error("MONGODB connection failed!!!", err);
+    });
